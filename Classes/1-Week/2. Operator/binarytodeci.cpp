8104f96fdc1c22f5d8606bf32a0921509e8dfb74
Ankan Bera <beraankan8@gmail.com>
2024-06-04 14:00:07 +0530

    added .

--- Classes/1-Week/2. Operator/binarytodeci.cpp
+++ Classes/1-Week/2. Operator/binarytodeci.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 using namespace std;
 int binarytodecimal(int n){
+     int i=1, bit = 0;
     while(n>0){
-     int bit=n%2;
-    cout<<bit<<endl;
+     int rem=n%2;
+     bit += rem * i;
     n=n/2;
+    i *= 10;
     }
+    return bit;
 }
 int main() {
     int n;
+    cout<<"Enter a number - ";
     cin>>n;
-   int b= binarytodecimal(n);
-   
+    int res = binarytodecimal(n);
+    cout<<res<<" ";
+return 0;
 }
