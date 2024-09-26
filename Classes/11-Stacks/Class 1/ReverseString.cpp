1e57cb448bb3b66fc5299e69a76a6368b16e783f
Ankan Bera <beraankan8@gmail.com>
2024-09-26 11:01:29 +0530

    Code modify

--- Classes/11-Stacks/Class 1/ReverseString.cpp
+++ Classes/11-Stacks/Class 1/ReverseString.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main(){
     string str = "ankan";
     stack<char> st;
-
+    cout<<"Original String is ->"<<endl;
+    cout<<str<<endl;
     for(int i=0; i<str.length(); i++){
         st.push(str[i]);
     }
-    //
+    cout<<"Reverse String is ->"<<endl;
     while(!st.empty()){
         cout<<st.top()<<" ";
         st.pop();
